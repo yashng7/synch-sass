dbaf625d9c5d521f3a1dd3e6a06d0b246b79756f
yashng7 <gawandeyashwant7@gmail.com>
2024-11-27 10:35:00 +0530

    add auth

---
+++ src/app/(auth)/sign-in/[[...rest]]/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { SignIn } from "@clerk/nextjs"
+import { useSearchParams } from "next/navigation"
+
+const Page = () => {
+  const searchParams = useSearchParams()
+  const intent = searchParams.get("intent")
+
+  return (
+    <div className="w-full flex-1 flex items-center justify-center">
+      <SignIn
+        forceRedirectUrl={intent ? `/dashboard?intent=${intent}` : "/dashboard"}
+      />
+    </div>
+  )
+}
+
+export default Page
---
+++ src/app/(auth)/sign-up/[[...rest]]/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { SignUp } from "@clerk/nextjs"
+
+const Page = () => {
+  return (
+    <div className="w-full flex-1 flex items-center justify-center">
+      <SignUp fallbackRedirectUrl="/welcome" forceRedirectUrl="/welcome" />
+    </div>
+  )
+}
+
+export default Page
