01e89d21f1ac173ce8ca13d51319297fc89375de
yashng7 <gawandeyashwant7@gmail.com>
2024-12-01 09:30:00 +0530

    add lib and hooks

---
+++ src/lib/stripe.ts
@@ -0,0 +1,32 @@
+import Stripe from "stripe"
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
+  apiVersion: "2024-09-30.acacia",
+  typescript: true,
+})
+
+export const createCheckoutSession = async ({
+  userEmail,
+  userId,
+}: {
+  userEmail: string
+  userId: string
+}) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price: "price_1QeZaLQ2OyBozPh7V9FnJKIx",
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: `${process.env.NEXT_PUBLIC_APP_URL}/dashboard?success=true`,
+    cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/pricing`,
+    customer_email: userEmail,
+    metadata: {
+      userId,
+    },
+  })
+
+  return session
+}
